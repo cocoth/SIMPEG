700629b5abf7540df7aaff7ac9075417a8b33e5e
cocoth <anonymousid0000000@gmail.com>
2024-04-19 21:58:30 +0700

    add integrations to database (read)

--- src/app/absensi/page.tsx
+++ src/app/absensi/page.tsx
@@ -3,10 +3,14 @@ import { DataTable } from "@/components/tables/DataTable"
 import { data } from "../data-pegawai/data"
 
 
-const AbsensiPage = () => {
+const AbsensiPage = async () => {
+    const url = process.env.BASE_URL
+    const res = await fetch(`${url}/api`)
+    const resJson = await res.json()
+
     return (
         <main className="container mx-auto py-10">
-           <DataTable columns={columns} data={data}/>
+           <DataTable columns={columns} data={resJson}/>
         </main>
     )
 }
