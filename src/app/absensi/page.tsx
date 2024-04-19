dbc795189abb085ee9b7381e05f84a9d04db8e0d
Nur Muhammad Kevin <nurmkevin532@gmail.com>
2024-04-19 23:59:08 +0700

    Hallo Lur

--- src/app/absensi/page.tsx
+++ src/app/absensi/page.tsx
@@ -1,8 +1,12 @@
 import { columns } from "@/components/tables/Columns";
 import { DataTable } from "@/components/tables/DataTable";
 import { data } from "../data-pegawai/data";
+import axios from "axios";
 
-const AbsensiPage = () => {
+const AbsensiPage = async () => {
+  const fetchData = await axios.get(
+    "https://pf189nf4-3000.asse.devtunnels.ms/api"
+  );
   return (
     <main className="container mx-auto py-10">
       <DataTable columns={columns} data={data} />
