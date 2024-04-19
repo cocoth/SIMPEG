2cebb3789aacc8d39402bf65c6227219efb8e86e
Nur Muhammad Kevin <nurmkevin532@gmail.com>
2024-04-19 21:56:40 +0700

    P

--- src/app/data-pegawai/data.ts
+++ src/app/data-pegawai/data.ts
@@ -1,10 +1,10 @@
 import { dataPegawai } from "../../../type";
 
-export const data:dataPegawai[] = [...Array(10)].map((_,i)=>({
-    no: i+1,
-    nama: `suprianto ${i+1}`,
-    gender: "Laki-Laki",
-    no_telp: "098765432123456",
-    email: `suprianto.${i+1}@test.com`,
-    status: i === 3 ? 'Tidak Aktif' : 'Aktif'
-}))
+export const data: dataPegawai[] = [...Array(10)].map((_, i) => ({
+  no: i + 1,
+  nama: `suprianto ${i + 1}`,
+  gender: "Laki-Laki",
+  no_telp: "098765432123456",
+  email: `suprianto.${i + 1}@test.com`,
+  status: i === 3 ? "Tidak Aktif" : "Aktif",
+}));
