ca1683b5c1c479d52e889c74bc683d7d23da5d33
cocoth <anonymousid0000000@gmail.com>
2024-05-10 10:33:32 +0700

    ADDED: Input validation

--- src/app/api/v1/pegawai/route.ts
+++ src/app/api/v1/pegawai/route.ts
@@ -1,7 +1,38 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Pegawai, PrismaClient } from "@prisma/client";
+import { ZodError, z } from "zod";
 
 const prisma = new PrismaClient();
+
+const customDate = z.string().transform((value)=>{
+  return new Date(value).toISOString()
+})
+
+const pegawaiSchema = z.object({
+  nama: z.string({
+    required_error: "'nama' is required!",
+    invalid_type_error: "'nama' shoud be string!",
+  }),
+  gender: z.string().optional(),
+  alamat: z.string().min(5, {
+    message: "'alamat' shoudbe at least 5 letters long!",
+  }),
+  email: z.string().email({
+    message: "invalid email address",
+  }),
+  no_telp: z.string().min(10, {
+    message: "'nomer telefon' shoud be at least 10 digit",
+  }),
+  jabatan: z.string(),
+  tanggal_lahir: customDate,
+  status: z.string(),
+  tanggal_masuk: customDate,
+  gaji_pokok: z.number(),
+  tunjangan: z.number(),
+  potongan_gaji: z.number(),
+  kehadiran: z.number(),
+  cuti: z.number(),
+});
 
 export const GET = async (req: NextRequest) => {
   try {
@@ -19,12 +50,18 @@ export const GET = async (req: NextRequest) => {
 export const POST = async (req: NextRequest) => {
   try {
     const body: Pegawai = await req.json();
+    const validateData = pegawaiSchema.parse(body);
     const pegawai = await prisma.pegawai.create({
-      data: { ...body },
+      data: { ...validateData },
     });
     return NextResponse.json(pegawai, { status: 201 });
   } catch (error) {
-    return NextResponse.json({ error: "POST data error!" }, { status: 500 });
+    if (error instanceof ZodError)
+      return NextResponse.json(
+        { error: `please check your data again before submitting` },
+        { status: 400 }
+      );
+    return NextResponse.json({ error: `POST data error!` }, { status: 400 });
   }
 };
 
@@ -40,7 +77,7 @@ export const PATCH = async (req: NextRequest) => {
     });
     return NextResponse.json(pegawai, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error: "PATCH data error!" }, { status: 500 });
+    return NextResponse.json({ error: "PATCH data error!" }, { status: 400 });
   }
 };
 
@@ -55,6 +92,6 @@ export const DELETE = async (req: NextRequest) => {
     });
     return NextResponse.json(pegawai, { status: 204 });
   } catch (error) {
-    return NextResponse.json({ error: "error to delete!" }, { status: 500 });
+    return NextResponse.json({ error: "error to delete!" }, { status: 400 });
   }
 };
